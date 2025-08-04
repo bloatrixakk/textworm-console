0649551e03d91c663dc357cde6e19bf784cbc11f
bloatrixakk <80479592+bloatrixakk@users.noreply.github.com>
2025-08-04 11:53:20 +0300

    fix autoUpdate error, testing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { autoUpdater } = require("electron-updater");
+const {app, BrowserWindow, ipcMain} = require("electron");
+const {autoUpdater} = require("electron-updater");
 const path = require("path");
 const Store = require("electron-store").default;
 
@@ -14,100 +14,99 @@ autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = true;
 
 function getAssetPath(...paths) {
-  return isDev
-    ? path.join(__dirname, ...paths)
-    : path.join(process.resourcesPath, ...paths);
+    return isDev
+        ? path.join(__dirname, ...paths)
+        : path.join(process.resourcesPath, ...paths);
 }
 
 function getPreloadPath(file) {
-  return path.join(__dirname, file); // preload lives in app.asar
+    return path.join(__dirname, file); // preload lives in app.asar
 }
 
 
 function createWindow(htmlPath, preloadPath, width = 1000, height = 800) {
-  const win = new BrowserWindow({
-    width,
-    height,
-    webPreferences: {
-      preload: preloadPath,
-      contextIsolation: true,
-      nodeIntegration: false,
-      sandbox: false
-    },
-    show: false,
-  });
+    const win = new BrowserWindow({
+        width,
+        height,
+        webPreferences: {
+            preload: preloadPath,
+            contextIsolation: true,
+            nodeIntegration: false,
+            sandbox: false
+        },
+        show: false,
+    });
 
-  win.loadFile(htmlPath);
+    win.loadFile(htmlPath);
 
-  win.once("ready-to-show", () => {
-    win.show();
-  });
+    win.once("ready-to-show", () => {
+        win.show();
+    });
 
-  // win.loadURL("http://localhost:3000/");
+    // win.loadURL("http://localhost:3000/");
 }
 
 app.whenReady().then(() => {
-  createWindow(
-    getAssetPath("build", "index.html"),
-    getPreloadPath("preload.js")
-  );
-  autoUpdater.checkForUpdates();
+    createWindow(
+        getAssetPath("build", "index.html"),
+        getPreloadPath("preload.js")
+    );
+    autoUpdater.checkForUpdates();
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+    if (process.platform !== "darwin") app.quit();
 });
 
 autoUpdater.on("update-available", (info) => {
-  const updateWindow = createWindow(
-    getAssetPath("build", "update", "update.html"),
-    getPreloadPath("updatePreload.js"),
-    400,
-    400
-  );
+    const updateWindow = createWindow(
+        getAssetPath("build", "update", "update.html"),
+        getPreloadPath("updatePreload.js"),
+        400,
+        400
+    );
 
-  function showMessage(message) {
-    console.log(message);
-    updateWindow.webContents.send("updateMessage", message);
-  }
-  
+    updateWindow.webContents.once("did-finish-load", () => {
+        updateWindow.webContents.send(
+            "updateMessage",
+            `Update Available! \n v${info.version} will be installed when you close the application. \n ${info.releaseName} \t ${info.releaseDate} \n${info.releaseNotes}`
+        );
+    })
 
-  showMessage(
-    `Update Available! \n v${info.version} will be installed when you close the application.`
-  );
+    autoUpdater.downloadUpdate();
 });
 
 autoUpdater.on('update-not-available', (info) => {
-  log.info('Update not available:', info);
+    log.info('Update not available:', info);
 });
 
 autoUpdater.on('error', (err) => {
-  log.error('Error in auto-updater:', err);
+    log.error('Error in auto-updater:', err);
 });
 
 autoUpdater.on('download-progress', (progressObj) => {
-  log.info(`Download speed: ${progressObj.bytesPerSecond}`);
-  log.info(`Downloaded ${progressObj.percent}%`);
+    log.info(`Download speed: ${progressObj.bytesPerSecond}`);
+    log.info(`Downloaded ${progressObj.percent}%`);
 });
 
 autoUpdater.on('update-downloaded', (info) => {
-  log.info('Update downloaded');
-  autoUpdater.quitAndInstall();
+    log.info('Update downloaded');
+    autoUpdater.quitAndInstall();
 });
 
 autoUpdater.on('checking-for-update', () => {
-  log.info('Checking for update...');
+    log.info('Checking for update...');
 });
 
 // --------- IPC HANDLERS ---------
 ipcMain.handle("save-token", (event, token) => {
-  store.set("authToken", token);
+    store.set("authToken", token);
 });
 
 ipcMain.handle("get-token", () => {
-  return store.get("authToken") || null;
+    return store.get("authToken") || null;
 });
 
 ipcMain.handle("delete-token", () => {
-  store.delete("authToken");
+    store.delete("authToken");
 });
